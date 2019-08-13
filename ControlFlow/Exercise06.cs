cdf41d0be727a60914b0ee48959bd82f6ad065e6
Ryan Silva <RyandotSilva@gmail.com>
2019-08-13 19:22:19 -0400

    Add exercise 07

--- ControlFlow/Exercise06.cs
+++ ControlFlow/Exercise06.cs
@@ -10,7 +10,7 @@ namespace ControlFlow
     // Write a program and continuously ask the user to enter a number or "ok" to exit.
     // Calculate the sum of all the previously entered numbers and display it on the console
     {
-        static void Main(string[] args)
+        public void Exercise()
         {
             var sum = 0;
 
---
+++ ControlFlow/Exercise07.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlFlow
+{
+    
+    class Exercise07
+    // Write a program and ask the user to enter a number.
+    // Compute the factorial of the number and print it on the console.
+    // For example, if the user enters 5, the program should calculate 5 x 4 x 3 x 2 x 1 and display it as 5! = 120.
+    {
+        public void Exercise()
+        {
+            Console.Write("Enter number to get factorial: ");
+
+            var number = Convert.ToInt32(Console.ReadLine());
+
+            var factorial = 1;
+
+            for (var i = 1; i <= number; i++)
+                factorial *= i;
+
+            Console.WriteLine("{0} is the factorial of {1}", factorial, number);
+        }
+    }
+}
