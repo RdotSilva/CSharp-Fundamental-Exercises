f72dbbe99d0c269cb7b58ae9a7ee6756e0d5d7b7
Ryan Silva <RyandotSilva@gmail.com>
2019-08-13 18:24:41 -0400

    Add exercise 02

--- ControlFlow/Exercise01.cs
+++ ControlFlow/Exercise01.cs
@@ -11,10 +11,13 @@ namespace ControlFlow
         // Write a program and ask the user to enter a number. The number should be between 1 to 10.
         // If the user enters a valid number, display "Valid" on the console. Otherwise, display "Invalid".
         // (This logic is used a lot in applications where values entered into input boxes need to be validated.)
-        static void Main(string[] args)
+        // static void Main(string[] args)
+        public void Exercise()
         {
             Console.Write("Enter a number from 1-10: ");
+
             var number = Convert.ToInt32(Console.ReadLine());
+
             if (number > 0 && number <= 10)
                 Console.WriteLine("Valid number entered");
             else
---
+++ ControlFlow/Exercise02.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlFlow
+{
+    class Exercise02
+    {
+        public void Exercise()
+        {
+            Console.Write("Enter 1st number: ");
+            var num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter 2nd number: ");
+            var num1 = Convert.ToInt32(Console.ReadLine());
+
+            var maxNumber = (num1 > num2) ? num1 : num2;
+            if (num1 == num2)
+                Console.WriteLine("Numbers are the same.");
+            else
+                Console.WriteLine("Max is " + maxNumber);
+        }
+    }
+}
